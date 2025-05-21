3aa08901ff8545ec3c90ba84599759ad5e612146
Phinxaura <yashrajput2808@gmail.com>
2025-05-21 11:29:09 +0530

    final commit

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -1,9 +1,9 @@
 import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
-  theme: localStorage.getItem("jimsify-theme") || "coffee",
+  theme: localStorage.getItem("streamify-theme") || "coffee",
   setTheme: (theme) => {
-    localStorage.setItem("jimsify-theme", theme);
+    localStorage.setItem("streamify-theme", theme);
     set({ theme });
   },
 }));
